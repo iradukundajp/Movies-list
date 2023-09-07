5861bd9d79b9d7770b7f34ada1e25b279c335b2a
iradukundajp <ikundajp@gmail.com>
2023-09-07 13:51:04 +0200

    update api

--- apis/addMovie.js
+++ apis/addMovie.js
@@ -1,59 +1,25 @@
-// import dataUrl from '../src/data.js';
+import dataUrl from '../src/data.js';
 
-// //const baseUrl = 'https://my-json-server.typicode.com/iradukundajp/Movies-list/movies';
-
-// const handleResponse = async (response) => {
-//     if (!response.ok) {
-//         throw new Error(`Request failed with status: ${response.status}`);
-//     }
-//     return await response.json();
-// };
-
-// const addMovie = async (method, data = null) => {
-//     try {
-//         const headers = {
-//             'Content-Type': 'application/json'
-//         };
-//         let body;
-
-//         if (method === 'POST') {
-//             body = JSON.stringify(data);
-//         }
-
-//         const response = await fetch(dataUrl.baseUrl, {
-//             method,
-//             headers,
-//             body
-//         });
-
-//         return await handleResponse(response);
-//     } catch (err) {
-//         console.error(err);
-//         return null;
-//     }
-// };
-
-// export default addMovie;
-import data from '../src/data.js';
-
-const addMovie = async (bodyData) => {
+const addMovie = async (method, data = null) => {
     try {
-        const res = await fetch(data.baseUrl, {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify(bodyData)
-        });
-        if (!res.ok) {
-            throw new Error(
-                `Failed to fetch movies with status : ${res.status}`
-            );
+        const headers = {
+            'Content-Type': 'application/json'
+        };
+        let body;
+
+        if (data !== null) {
+            body = JSON.stringify(data);
         }
-        return await res.json();
-    } catch (error) {
-        console.log(error);
-        return null;
+
+        await fetch(dataUrl.baseUrl, {
+            method,
+            headers,
+            body
+        });
+
+        // No need to handle the response data
+    } catch (err) {
+        console.error(err);
     }
 };
 
